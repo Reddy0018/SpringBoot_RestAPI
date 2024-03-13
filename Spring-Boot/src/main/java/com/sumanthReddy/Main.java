2f1d468eb3f2e2b8e822af9ead35ea1c22117ef0
Sumanth Reddy Bekkem <sxb1454@case.edu>
2024-03-13 15:28:48 -0400

    Commented code

--- Spring-Boot/src/main/java/com/sumanthReddy/Main.java
+++ Spring-Boot/src/main/java/com/sumanthReddy/Main.java
@@ -6,14 +6,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
+//@RestController
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Sumanth Reddy");
         SpringApplication.run(Main.class,args);
     }
 
-    @GetMapping("/")
+//    @GetMapping("/")
     public greeting greet(){
         return new greeting("Hi Welcome");
     }
